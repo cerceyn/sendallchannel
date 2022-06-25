4fa49555c9cd7080f0250d64d3940fe73d0b88af
cerceyn <104266528+cerceyn@users.noreply.github.com>
2022-06-25 16:50:03 +0300

    Create __main__.py

---
+++ android/__main__.py
@@ -0,0 +1,56 @@
+import asyncio
+import base64
+from subprocess import PIPE, Popen
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.messages import AddChatUserRequest
+from telethon.tl.functions.channels import InviteToChannelRequest
+from time import sleep
+from android import *
+from . import console
+
+Token="MTc1ODU4MTE4NTpBQUc4YlQteTFPMDJNRW5EMmlqR3hzRGx6MXE3dEMyZWR1TQ=="
+bot=None
+async def botagir():
+    data = [1,2,3,4]
+    with console.status("[bold blue] Bota girme işlemi sürüyor...") as status:
+        while data:
+            num = data.pop(0)
+            sleep(2)
+            if num==1:
+                console.log(f"[cyan] Token ayarlanıyor...[/cyan]")
+                Token = base64.b64decode(Token)
+            elif num==2:
+                console.log(f"[cyan] Api bilgileri ayrıştırılıyor...[/cyan]")
+                u = base64.b64decode("NzhkNDgzNmI2MjNlMDZkZWNlNTIwMzMxMTRiZGIyMWV8fDEzMzEyNDE4")
+                try:
+                   bot = TelegramClient('bots',u.split("||")[1], u.split("||")[0])
+                except Exception as e:
+                   hata(f"Bir sorunla karşılaştık! Bu hatayı geliştiriciye bildirin:\n{str(e)}")
+            elif num==3:
+                console.log(f"[cyan] Giriş yapılıyor...[/cyan]")
+                console.log(f"[red] Hata alınması en muhtemel yer...[/red]")
+                try:
+                   await bot.start(bot_token=Token)
+                except Exception as e:
+                   hata(f"Bir sorunla karşılaştık! Bu hatayı geliştiriciye bildirin:\n{str(e)}")
+            elif num==4:
+                console.log(f'[bold][red]Tamam!')
+                await bot.disconnect()
+
+async def main ():
+    logo(True)
+    await botagir()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        hata("Güle güle!")
+        #loop.run_until_complete(disconn(userbot))
+
+
+
+
+
